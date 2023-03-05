587e0584ad1494fd7c1035ed5b90498f1e4a35a9
Edwaldo Almeida <edwaldojunior@icloud.com>
2023-03-04 21:25:08 -0500

    pushing partial solution (still need to make the TIE to work)

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/TeamUtils.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/TeamUtils.java
@@ -1,12 +1,10 @@
 package com.linkedin.javacodechallenges;
 
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class TeamUtils {
 
-  public static void generateTeamsScores(List<Team> teams,
-      int numberOfRounds) {
+  public static void generateTeamsScores(List<Team> teams, int numberOfRounds) {
     Random random = new Random();
     teams.forEach(team -> {
       for (int i = 0; i < numberOfRounds; i++) {
@@ -16,6 +14,59 @@ public class TeamUtils {
   }
 
   public static void revealResults(List<Team> teams) {
+    Map<Team, Integer> results = new HashMap<>();
+    Map<Team, Integer> orderedResults = new TreeMap<>(new ValueComparator(results));
 
+    if (teams.isEmpty()) {
+      System.out.println("The game hasn't started yet.");
+      return;
+    }
+
+    // check if there is no scores
+    for (Team team: teams) {
+      if (team.sumTotalScore() > 0) {
+        ;
+      } else {
+        System.out.println("The game hasn't started yet.");
+        return;
+      }
+    }
+
+    for (Team team: teams) {
+      results.put(team, team.sumTotalScore());
+    }
+    orderedResults.putAll(results);
+
+    System.out.println("Now for the results, the WINNER is...");
+      boolean firstResult = true;
+      for (Team team: orderedResults.keySet()){
+        if (firstResult) {
+          System.out.println("With " + team.sumTotalScore() + " points, it's team " + team.getPlayer1() + " and " + team.getPlayer2() + "!\n");
+          firstResult = false;
+        } else {
+          System.out.println("Then we have... ");
+          System.out.println("With " + team.sumTotalScore() + " points, it's team " + team.getPlayer1() + " and " + team.getPlayer2() + "!\n");
+        }
+      }
   }
+
+  static class ValueComparator implements Comparator<Team> {
+    Map<Team, Integer> map;
+
+    public ValueComparator(Map<Team, Integer> map) {
+      this.map = map;
+    }
+
+    @Override
+    public int compare(Team a, Team b) {
+//      if (map.get(a) >= map.get(b)) {
+      if (map.get(a) <= map.get(b)) {
+        return 1;
+      } else {
+        return -1;
+      }
+    }
+  }
+
+
 }
