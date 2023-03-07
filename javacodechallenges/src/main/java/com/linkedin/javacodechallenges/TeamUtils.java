f53f28ae269f7c1660b7d6a3f0e9a197c7e9db12
Edwaldo Almeida <edwaldojunior@icloud.com>
2023-03-06 21:19:06 -0500

    all tests working, but tiedNonWinner not yet

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/TeamUtils.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/TeamUtils.java
@@ -22,32 +22,69 @@ public class TeamUtils {
       return;
     }
 
-    // check if there is no scores
     for (Team team: teams) {
       if (team.sumTotalScore() > 0) {
-        ;
-      } else {
-        System.out.println("The game hasn't started yet.");
-        return;
+        results.put(team, team.sumTotalScore());
       }
     }
 
-    for (Team team: teams) {
-      results.put(team, team.sumTotalScore());
-    }
-    orderedResults.putAll(results);
+    if (results.isEmpty()) {
+      System.out.println("The game hasn't started yet.");
+    } else {
+      orderedResults.putAll(results);
+      System.out.println("Now for the results, the WINNER is...");
 
-    System.out.println("Now for the results, the WINNER is...");
-      boolean firstResult = true;
-      for (Team team: orderedResults.keySet()){
-        if (firstResult) {
-          System.out.println("With " + team.sumTotalScore() + " points, it's team " + team.getPlayer1() + " and " + team.getPlayer2() + "!\n");
-          firstResult = false;
-        } else {
-          System.out.println("Then we have... ");
-          System.out.println("With " + team.sumTotalScore() + " points, it's team " + team.getPlayer1() + " and " + team.getPlayer2() + "!\n");
+      // check the winner(s) and print the results in order
+//      boolean firstResult = true;
+//      for (Team team: orderedResults.keySet()){
+//        if (firstResult) {
+//          System.out.println("With " + team.sumTotalScore() + " points, it's team " + team.getPlayer1() + " and " + team.getPlayer2() + "!\n");
+//          firstResult = false;
+//        } else {
+//          System.out.println("Then we have... ");
+//          System.out.println("With " + team.sumTotalScore() + " points, it's team " + team.getPlayer1() + " and " + team.getPlayer2() + "!\n");
+//        }
+//      }
+
+      // check the winners (new approach)
+      Iterator<Map.Entry<Team, Integer>> iterator = orderedResults.entrySet().iterator();
+      // TODO: include a while i < something.lenght() - 2 ???
+      int i = 0;
+      Map.Entry<Team, Integer> currentEntry = null;
+      Map.Entry<Team, Integer> nextEntry = null;
+      while (i <= orderedResults.size()) {
+        if (iterator.hasNext()) {
+          currentEntry = iterator.next();
+          if (iterator.hasNext()) {
+            nextEntry = iterator.next();
+            // if it's a TIE, print the current and next together:
+            if (currentEntry.getKey().sumTotalScore() == nextEntry.getKey().sumTotalScore()) {
+              System.out.println("It's a TIE!");
+              System.out.println("With " + currentEntry.getKey().sumTotalScore() + " points, it's team " +
+                      currentEntry.getKey().getPlayer1() + " and " +
+                      currentEntry.getKey().getPlayer2() + "!");
+              System.out.println("With " + nextEntry.getKey().sumTotalScore() + " points, it's team " +
+                      nextEntry.getKey().getPlayer1() + " and " +
+                      nextEntry.getKey().getPlayer2() + "!\n");
+            } else { // print only the current and let the execution go
+              System.out.println("With " + currentEntry.getKey().sumTotalScore() + " points, it's team " +
+                      currentEntry.getKey().getPlayer1() + " and " +
+                      currentEntry.getKey().getPlayer2() + "!\n");
+              System.out.println("Then we have... ");
+              System.out.println("With " + nextEntry.getKey().sumTotalScore() + " points, it's team " +
+                      nextEntry.getKey().getPlayer1() + " and " +
+                      nextEntry.getKey().getPlayer2() + "!\n");
+            }
+            System.out.println("Then we have... ");
+          } else {
+            System.out.println("With " + currentEntry.getKey().sumTotalScore() + " points, it's team " +
+                    currentEntry.getKey().getPlayer1() + " and " +
+                    currentEntry.getKey().getPlayer2() + "!\n");
+          }
         }
+        i += 1;
       }
+    }
   }
 
   static class ValueComparator implements Comparator<Team> {
